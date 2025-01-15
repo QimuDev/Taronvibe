4ed6e57f54ec8a2094b0582bc5b75093260c852c
Maralde <marcosallet@gmail.com>
2025-01-15 13:15:04 +0100

    añadido footer

--- listadoOrganizadores.php
+++ listadoOrganizadores.php
@@ -19,12 +19,10 @@ if($result -> num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista organizadores</title>
     <link rel="stylesheet" href="./css/listadoOrganizadores.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@400..800&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <nav>
         <div class="nav-logo">
@@ -58,5 +56,49 @@ if($result -> num_rows > 0) {
             <?=$lista?>
         </div>
     </div>
+
+    <div class="marquee-container">
+        <div class="marquee">
+            <span>Una súper frase animada scrolling non stop, Una súper frase animada non stop, Una súper frase animada scrolling non stop, Una súper frase animada non stop, Una súper frase animada scrolling non stop, Una súper frase animada non stop, </span>
+        </div>
+    </div>
+    
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-left">
+                <img src="./img/listadoOrg/taronvibeLogo.png" alt="Logo TaronVibe" class="logo">
+                <ul class="footer-links">
+                    <li><a href="#">Eventos</a></li>
+                    <li><a href="#">Organizadores</a></li>
+                    <li><a href="#">Iniciar sesión</a></li>
+                </ul>
+                <div class="footer-social">
+                    <span class="circle"></span>
+                    <span class="circle"></span>
+                    <span class="circle"></span>
+                </div>
+            </div>
+            <div class="footer-right">
+                <h2>Únete a nuestra newsletter</h2>
+                <form class="newsletter-form">
+                    <div class="form-input">
+                        <input type="text" placeholder="Nombre" class="input">
+                        <input type="email" placeholder="Correo electrónico" class="input">
+                    </div>
+                    <div class="form-btn">
+                        <button type="submit" class="btn-footer">Suscribete</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Copyright © 2025 - TARONVIBE</p>
+            <ul class="footer-policies">
+                <li><a href="#">Política de Privacidad</a></li>
+                <li><a href="#">Política de Cookies</a></li>
+                <li><a href="#">Aviso Legal</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
