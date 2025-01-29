a05036b205822d1e16044b890681902a8391bf41
Maralde <marcosallet@gmail.com>
2025-01-29 10:26:23 +0100

    modificado partes estéticas

--- detalleOrganizador.php
+++ detalleOrganizador.php
@@ -18,7 +18,7 @@ if(isset($_GET['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$nombre;?></title>
     <link rel="stylesheet" href="./css/detalleOrganizador.css">
 </head>
 <body>
